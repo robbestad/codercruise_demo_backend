ec219ac2ebcf98393d6588a34624bcf960c65dd0
Sven Anders Robbestad <sven@robbestad.com>
2017-07-17 15:43:50 -0500

    Add before/after

--- test/fetchImages.js
+++ test/fetchImages.js
@@ -1,22 +1,30 @@
-const MongoClient = require('mongodb').MongoClient
-const url = 'mongodb://localhost:27017/codercruise'
+const MongoClient = require("mongodb").MongoClient
+const url = "mongodb://localhost:27017/codercruise"
 const getImages = require("../src/index")
-const assert = require("chai").assert;
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-const should = chai.should();
-
+const chai = require("chai")
+const chaiAsPromised = require("chai-as-promised")
+chai.use(chaiAsPromised)
+const should = chai.should()
 
 let db = null
 
-MongoClient.connect(url, (err, _db) => {
-    if (err) throw err
-    db = _db
-})
+describe("Mongo test", function () {
 
-describe('Mongo test', function () {
-    it('should return an array of data', function () {
-        return getImages(db).then(data => data).should.eventually.have.length(2)
+    before(function (done) {
+        MongoClient.connect(url, (err, _db) => {
+            if (err) throw err
+            db = _db
+            done()
+        })
     });
-});
+
+    it("should return an array of data", function () {
+        return getImages(db).then(data => data).should.eventually.have.length(2)
+    })
+
+    after(function (done) {
+        db.close()
+        done()
+    })
+
+})
